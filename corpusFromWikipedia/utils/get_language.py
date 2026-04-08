49c90cbd3bb14bce8cfa8850e9e80caaad22ec80
gon-zalo <gonzsan02@gmail.com>
2026-04-08 11:33:08 +0000

    support for ISO 3 letter codes

--- corpusFromWikipedia/utils/get_language.py
+++ corpusFromWikipedia/utils/get_language.py
@@ -5,7 +5,16 @@ def get_language(language):
         language_pyc = pycountry.languages.get(alpha_2=language)
         language_code = language_pyc.alpha_2
         language_name = language_pyc.name
-    elif len(language) > 2: # if language is the name of the language
+
+    elif len(language) == 3:
+        language_pyc = pycountry.languages.get(alpha_3=language)
+
+        if language_pyc:
+            language_code = language_pyc.alpha_2 if hasattr(language_pyc, 'alpha_2') else language_pyc.alpha_3
+
+        language_name = language_pyc.name
+        
+    elif len(language) > 3: # if language is the name of the language
         for lang in pycountry.languages:
             if lang.name.lower() == language.lower():
                 language_pyc = pycountry.languages.get(name=language)
